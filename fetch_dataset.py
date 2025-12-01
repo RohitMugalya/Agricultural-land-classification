3cf3fb7892e900205efa976a643540c1f9022fa2
Rohit Mugalya <132080109+RohitMugalya@users.noreply.github.com>
2025-12-01 14:33:13 +0530

    initial commit

---
+++ fetch_dataset.py
@@ -0,0 +1,61 @@
+extract_dir = "."
+
+url = "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/4Z1fwRR295-1O3PMQBH6Dg/images-dataSAT.tar"
+
+
+def check_skillnetwork_extraction(extract_dir):
+    """ function to check whether data download and extraction method 
+    `skillsnetwork.prepare` would execute successfully, without downloading any data.
+    This helps in early detection and fast fallback to explicit download and extraction
+    using default libraries
+    ###This is a hack for the code to run on non-cloud computing environment without errors
+    """
+    symlink_test = os.path.join(extract_dir, "symlink_test")
+    if not os.path.exists(symlink_test):
+        os.symlink(os.path.join(os.sep, "tmp"), symlink_test) 
+        print("Write permissions available for downloading and extracting the dataset tar file")
+    os.unlink(symlink_test)
+
+
+async def download_tar_dataset(url, tar_path, extract_dir):
+    """function to explicitly download and extract the dataset tar file from cloud using native python libraries
+    """
+    if not os.path.exists(tar_path): # download only if file not downloaded already
+        try:
+            print(f"Downloading from {url}...")
+            async with httpx.AsyncClient() as client:
+                response = await client.get(url, follow_redirects=True)# Download the file asynchronously
+                response.raise_for_status() # Raise an exception for bad status codes (4xx or 5xx)
+            
+                with open(tar_path , "wb") as f:
+                    f.write(response.content) # Save the downloaded file
+                print(f"Successfully downloaded '{file_name}'.")
+        except httpx.HTTPStatusError as http_err:
+            print(f"HTTP error occurred during download: {http_err}")
+        except Exception as download_err:
+            print(f"An error occurred during the fallback process: {download_err}")
+    else:
+        print(f"dataset tar file already downloaded at: {tar_path}")
+    with tarfile.open(tar_path, 'r:*') as tar_ref:
+        tar_ref.extractall(path=extract_dir)
+    print(f"✅ Successfully extracted to '{extract_dir}'.")
+
+
+try:
+    check_skillnetwork_extraction(extract_dir)
+    await skillsnetwork.prepare(url = url, path = extract_dir, overwrite = True)
+except Exception as e:
+    print(e)
+    # --- FALLBACK METHOD FOR DOWNLOADING THE DATA ---
+    print("❌ Primary download/extration method failed.")
+    print("Falling back to manual download and extraction...")
+    
+    # import libraries required for downloading and extraction
+    import tarfile
+    import httpx 
+    from pathlib import Path
+    
+    file_name = Path(url).name
+    tar_path = os.path.join(extract_dir, file_name)
+    print(f"tar_path: {os.path.exists(tar_path)} ___ {tar_path}")
+    await download_tar_dataset(url, tar_path, extract_dir)
